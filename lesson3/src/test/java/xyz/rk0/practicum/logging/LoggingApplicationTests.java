7f086f851be16d2fc2e905d0065c77166294cb19
Ryan Kadri <johndoe@example.com>
2021-02-02 21:59:09 -0500

    Setup

---
+++ lesson3/src/test/java/xyz/rk0/practicum/logging/LoggingApplicationTests.java
@@ -0,0 +1,13 @@
+package xyz.rk0.practicum.logging;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LoggingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
